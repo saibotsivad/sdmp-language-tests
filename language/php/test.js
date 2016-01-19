1207e77e76a73879b1ce14cce14b5ac3670e886a
Tobias Davis <accounts@davistobias.com>
2016-01-19 01:08:42 -0600

    Added in test scaffold for JS and PHP, becausewhynot?!

--- language/php/test.js
+++ language/php/test.js
@@ -1,26 +1,53 @@
+/*
+Each language needs to implement the following functions:
+*/
 module.exports = {
-	makePublicKey: {
+	makePrivateKey: {
 		generate: function(callback) {
-			callback('generated key')
+			// generate a PEM encoded private key and give to callback as string
+			callback('the private key')
 		},
-		verify: function(key, callback) {
-			callback(false)
+		verify: function(privateKey, callback) {
+			// verify the given string is a PEM encoded private key
+			var thePrivateKeyIsValid = true
+			callback(thePrivateKeyIsValid)
 		}
-	},
-	signData: {
-		generate: function() {},
-		verify: function() {}
-	},
-	verifySignedData: {
-		generate: function() {},
-		verify: function() {}
-	},
-	encryptData: {
-		generate: function() {},
-		verify: function() {}
-	},
-	decryptData: {
-		generate: function() {},
-		verify: function() {}
+	}
+	, makePublicKey: {
+		generate: function(privateKey, callback) {
+			// given a PEM encoded private key, generate a PEM encoded
+			// public key and give to the callback as a string
+			callback('the public key')
+		},
+		verify: function(publicKey, callback) {
+			// verify the given string is a PEM encoded public key
+			var thePublicKeyIsValid = true
+			callback(thePublicKeyIsValid)
+		}
+	}
+	, signData: {
+		generate: function(privateKey, data, callback) {
+			// given a PEM encoded private key and some data, generate a signature
+			// and give to callback as string
+			callback('the signature')
+		},
+		verify: function(publicKey, originalData, signature, callback) {
+			// given a PEM encoded public key, verify the signature of the data
+			var signatureIsValid = true
+			callback(signatureIsValid)
+		}
+	}
+	, encryptData: {
+		generate: function(publicKey, data, callback) {
+			// given a PEM encoded public key and some data, encrypt the data
+			// to that key and give that response to the callback
+			callback('the encrypted data')
+		},
+		verify: function(privateKey, originalData, encrypted, callback) {
+			// given a PEM encoded private key and some encrypted data, decrypt
+			// it and make sure it is the same as the original data
+			var dataDecryptedAndIsSameAsOriginal = true
+			callback(dataDecryptedAndIsSameAsOriginal)
+		}
 	}
 }
